d77e73cef2de8fbb8a2eec810605ace4eebe33ce
Cara Kane <cara.kane@gmail.com>
2017-12-04 19:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Driver {
   yearsExperienceFromBeginningOf(year) {
     let now = new Date().getFullYear(year)
     console.log(now)
-    console.log(this.startDate))
+    console.log(this.startDate)
     return(now - this.startDate.getYear())
   }
 }
